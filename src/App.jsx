dd360ab3e39a7191205a7832ff4d6cbcf55a515b
Mathias Rendon <mathiassaid7@gmail.com>
2025-06-17 13:30:24 -0500

    feat: add layout with background colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
 function App() {
   return (
     <>
+    <main className="w-full min-h-dvh flex flex-col bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
+      <div className="header-container w-full">
+        <Header />
+      </div>
+      <div className="main-content flex-1">
+      </div>
+      <div className="footer-container w-full">
+        <Footer />
+      </div>
+    </main>
     </>
   )
 }
