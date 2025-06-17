b101f2579700aafab434f23093e4099f6b873ae5
CodeFoxZ37 <130168476+CodeFoxZ37@users.noreply.github.com>
2025-06-17 14:45:48 -0400

    Se agrego una nueva opcion en el formulario del componente  Message

--- src/components/Conctacs.jsx
+++ src/components/Conctacs.jsx
@@ -22,3 +22,5 @@ function Conctacs(){
         </>
     )
 }
+
+export default Conctacs;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -10,11 +10,19 @@ function Message(){
             <label>Email</label>
              <input type="email" />
             <label>Asunto</label>
-            <select>
+            <select className="selectAffair">
                 <Option value = "">Selecciona un tema</Option>
                 <Option value = "Question">Preguntas</Option>
                 <Option value = "Service">Servicios</Option>
+                <Option value = "suggestion">Sugerencias</Option>
             </select>
+            <select className="selectService">
+                <Option value = "">Selecciona un Servicio</Option>
+                <Option value = "Web">Sitio WEB</Option>
+                <Option value = "Bot-discord">Bot de discord</Option>
+                <Option value = "Other">Otros</Option>
+            </select>
+
             <label>Mensaje</label>
              <input type="text" />
             <button>Enviar mensaje</button>
