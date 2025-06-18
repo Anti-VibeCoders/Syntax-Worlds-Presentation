44e0be976def013f0a901b33f27a15866cf12e71
Mathias Rendon <mathiassaid7@gmail.com>
2025-06-18 17:36:48 -0500

    style: align text to center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
     return (
         <>
-        <footer className="flex h-12 px-8 items-center">
-            <span className="text-gray-400">&copy; Syntax World. Todos los derechos reservados</span>
+        <footer className="flex h-12 px-8 items-center justify-center">
+            <span className="text-gray-400 text-sm text-center">&copy; Syntax World. Todos los derechos reservados</span>
         </footer>
         </>
     )
