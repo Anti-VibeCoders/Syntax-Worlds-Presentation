ab8dcd8e5de239e8e9ef0ec159a4416f976d627b
Mathias Rendon <mathiassaid7@gmail.com>
2025-06-20 21:16:35 -0500

    fix: change label to their respective tag

--- src/components/Conctacs.jsx
+++ src/components/Conctacs.jsx
@@ -2,26 +2,26 @@ import Message from "./Message";
 import FastConctacs from "./FastConctacs";
 import WorkingHours from "./WorkingHours";
 
-function Conctacs(){
-    return(
-     <>
-     <div className="flex flex-col gap-4 items-center justify-center w-full py-20"> 
-        <div className="flex flex-col gap-4 text-center w-full items-center overflow-hidden">
-            <label className="text-5xl font-bold bg-gradient-to-r from-purple-300 via-white to-purple-300 bg-clip-text text-transparent text-center max-lg:text-4xl max-md:text-3xl max-sm:text-3xl">Cóntactanos</label>
-            <p className="text-sm text-slate-300 font-inter max-w-[70ch] mx-auto text-center max-sm:px-4">¿Tienes preguntas, sugerencias o quiere colaborar con nosotros? estamos aquí para ayudarte y escuchar tus ideas.</p>
-        </div>
-     <div className=" max-xl:px-6 max-md:flex-col max-md:items-center flex justify-center gap-8 text-white font-inter w-full"> 
-        <div className="max-md:w-full">
-            <Message/>
-        </div>
-        <div className="flex flex-col gap-4 items-center w-sm max-md:w-full">
-            <FastConctacs/>
-            <WorkingHours/>
-        </div>
-     </div>
-     </div>
-     
-      </>
+function Conctacs() {
+    return (
+        <>
+            <div className="contact-container flex flex-col gap-8 items-center justify-center w-full py-20" id="contact-container">
+                <div className="flex flex-col gap-4 text-center w-full items-center overflow-hidden">
+                    <h2 className="text-5xl font-bold bg-gradient-to-r from-purple-300 via-white to-purple-300 bg-clip-text text-transparent text-center max-lg:text-4xl max-md:text-3xl max-sm:text-3xl ;leading-normal">Cóntactanos</h2>
+                    <p className="text-base max-md:text-base text-slate-300 font-inter max-w-[70ch] mx-auto text-center max-sm:px-4">¿Tienes preguntas, sugerencias o quiere colaborar con nosotros? estamos aquí para ayudarte y escuchar tus ideas.</p>
+                </div>
+                <div className=" max-xl:px-6 max-md:flex-col max-md:items-center flex justify-center gap-8 text-white font-inter w-full">
+                    <div className="max-md:w-full w-2xl">
+                        <Message />
+                    </div>
+                    <div className="flex flex-col gap-4 items-center w-sm max-md:w-full">
+                        <FastConctacs />
+                        <WorkingHours />
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
--- src/components/WorkingHours.jsx
+++ src/components/WorkingHours.jsx
@@ -1,21 +1,21 @@
-function WorkingHours(){
-    return(
+function WorkingHours() {
+    return (
         <>
-          <div className=" max-md:w-full flex flex-col gap-4 bg-slate-800/50 backdrop-blur-sm border-slate-700/50 border text-white rounded-2xl p-5 w-full h-full font-inter">
-            <b className="text-xl">Horarios de atención</b>
-            <div className="flex justify-between text-sm">
-                <label className="text-[#7a7396]">Lunes-Viernes</label><b>9:00 - 18:00 UTC</b>
+            <div className=" max-md:w-full flex flex-col gap-4 bg-slate-800/50 backdrop-blur-sm border-slate-700/50 border text-white rounded-2xl p-5 w-full h-full font-inter">
+                <b className="text-xl">Horarios de atención</b>
+                <div className="flex justify-between text-sm">
+                    <span className="text-[#7a7396]">Lunes-Viernes</span><b>9:00 - 18:00 UTC</b>
+                </div>
+                <div className="flex justify-between text-sm">
+                    <span className="text-[#7a7396]">Sabado</span><b>10:00 - 16:00 UTC</b>
+                </div>
+                <div className="flex justify-between text-sm text-[#7a7396]">
+                    <span>Domingo</span><span className="text-red-500 font-semibold">Cerrado</span>
+                </div>
+                <div className="bg-[#3c2b64] border border-[#7f57a7] rounded-lg px-4 py-2 flex justify-center items-center">
+                    <p className="text-xs leading-normal text-[#c290f4]">💡 Para respuestas más rapidas, únete a nuestro Discord donde la actividad está activa 24/7.</p>
+                </div>
             </div>
-            <div className="flex justify-between text-sm">
-                <label className="text-[#7a7396]">Sabado</label><b>10:00 - 16:00 UTC</b>
-            </div>
-            <div className="flex justify-between text-sm text-[#7a7396]">
-                <label>Domingo</label><label>Cerrado</label>
-            </div>
-            <div className="bg-[#3c2b64] border border-[#7f57a7] rounded-lg px-2 py-1">
-                <label className="text-xs text-[#c290f4]">💡 Para respuestas más rapidas, únete a nuestro Discord donde la actividad está activa 24/7.</label>
-            </div>
-          </div>
         </>
     )
 }
